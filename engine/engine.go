58cfebdb834b0b63d271a9d33b009d7463d28f58
Didip Kerabat <didip@newrelic.com>
2015-06-07 22:13:42 -0700

    Engine is able to run Logic.

--- engine/engine.go
+++ engine/engine.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
 )
 
@@ -28,6 +29,7 @@ func New(root string) (*Engine, error) {
 	engine.Stacks = stackFiles
 	engine.Roles = roleFiles
 
+	engine.DryRun = true
 	engine.PythonPath = "/usr/bin/python"
 	engine.RubyPath = "/usr/bin/ruby"
 
@@ -38,6 +40,7 @@ type Engine struct {
 	Root       string
 	PythonPath string
 	RubyPath   string
+	DryRun     bool
 	Logic      []os.FileInfo
 	Stacks     []os.FileInfo
 	Roles      []os.FileInfo
@@ -45,4 +48,13 @@ type Engine struct {
 
 func (e *Engine) ReadDir(dirname string) ([]os.FileInfo, error) {
 	return ioutil.ReadDir(path.Join(e.Root, dirname))
+}
+
+func (e *Engine) RunLogic(name string) ([]byte, error) {
+	logicPath := path.Join(e.Root, "logic", name, "__init__.py")
+	if e.DryRun {
+		return []byte(e.PythonPath + " " + logicPath), nil
+	}
+
+	return exec.Command(e.PythonPath, logicPath).CombinedOutput()
 }
--- engine/engine_test.go
+++ engine/engine_test.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -15,5 +16,26 @@ func TestConstructor(t *testing.T) {
 	}
 	if engine.RubyPath == "" {
 		t.Fatalf("Engine Ruby path should not be empty.")
+	}
+	if engine.DryRun != true {
+		t.Fatalf("DryRun should be true bydefault.")
+	}
+}
+
+func TestRunLogic(t *testing.T) {
+	engine, err := New(os.ExpandEnv("$GOPATH/src/github.com/resourced/configurator/blank"))
+	if err != nil {
+		t.Fatalf("Creating new engine should not fail. Error: %v", err)
+	}
+
+	output, err := engine.RunLogic("ignore-me")
+	if err != nil {
+		t.Fatalf("RunLogic should not fail in dry run mode. Error: %v", err)
+	}
+	if !strings.Contains(string(output), "/python") {
+		t.Fatalf("RunLogic should contain python command in dry run mode. Output: %v", string(output))
+	}
+	if !strings.HasSuffix(string(output), "__init__.py") {
+		t.Fatalf("RunLogic should contain __init__.py in dry run mode. Output: %v", string(output))
 	}
 }
